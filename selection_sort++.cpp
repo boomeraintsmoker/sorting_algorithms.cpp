55924bc68ba8dee26476ba11b8104e1a3a45a52c
innit_boomer.cpp <117290144+boomeraintsmoker@users.noreply.github.com>
2022-11-06 17:59:09 +0300

    Selection sort
    One of the most inefficient sorting algorithms, but you need to know it. It is also simple enough to understand the sorting process.

---
+++ selection_sort++.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	const int N = 8;
+	int a[N] = { 5, 2, 1, 6, 4, 3, 7, 0 };
+
+	for (int i = 0; i < N; ++i)
+	{
+		int remaining_elems = i;
+
+		for (int j = i; j < N; ++j)
+		{
+			int buff = {};
+
+			if (a[j] < a[remaining_elems])
+			{
+				buff = a[j];
+				a[j] = a[remaining_elems];
+				a[remaining_elems] = buff;
+			}
+			else if (a[j] >= a[remaining_elems])
+			{
+				a[j] == a[remaining_elems];
+			}
+		}
+		cout << a[i] << " ";
+	}
+	return 0;
+}
